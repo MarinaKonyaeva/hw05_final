2c1254818207ee175b2fd3873ecd094638d79f99
Marina Konyaeva <lana_rzn2005@mail.ru>
2021-10-16 23:57:43 +0300

    DEBUG changed to FALSE for test custom error pages

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(wmqo+89r0wx*ee-9a9m-=&sk_u-(!x3i)_aq*@_wc^v5ne7#c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
